9236009a01c0b136fdace4c88b1c971cc1e7cf27
dldadnasjndfsjafajnsdfadnfadfkoafasdklk <43988221+dldadnasjndfsjafajnsdfadnfadfkoafasdklk@users.noreply.github.com>
2018-10-12 18:13:58 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+
+
+client.on('message', msg => {
+
+    if (msg.content == '69join') {
+        if (msg.member.voiceChannel) {
+
+     if (msg.member.voiceChannel.joinable) {
+         msg.member.voiceChannel.join().then(msg.react('✅'));
+     }
+    }
+}
+})
+client.login(process.env.token1)
